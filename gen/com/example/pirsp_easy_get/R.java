be97f8b9a950018771f5254c0f4e5d271a4168d7
shoguncao <shou_gang@163.com>
2014-04-02 00:24:19 +0800

    【变更说明】添加+号按钮，后面做用来跳转发表的按钮 【代码审核】shoguncao 【风险评估】小 【影响范围】 【自测验证】模拟器：OK

--- gen/com/example/pirsp_easy_get/R.java
+++ gen/com/example/pirsp_easy_get/R.java
@@ -22,12 +22,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_publish=0x7f020001;
     }
     public static final class id {
-        public static final int action_exit=0x7f080003;
-        public static final int action_settings=0x7f080002;
-        public static final int bitmapView=0x7f080001;
+        public static final int action_exit=0x7f080004;
+        public static final int action_settings=0x7f080003;
+        public static final int bitmapView=0x7f080002;
         public static final int btnOpenAlbum=0x7f080000;
+        public static final int imgViewPublis=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,6 +40,7 @@ public final class R {
     }
     public static final class string {
         public static final int action_menu=0x7f050003;
+        public static final int action_publish=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int open_album=0x7f050002;
